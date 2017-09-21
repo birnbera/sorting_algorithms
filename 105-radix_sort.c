aba73dae5f2f2c37608dce3ccee9c7c65e7c94fa
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-09-21 00:28:36 +0000

    radix sort style fix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -4,45 +4,45 @@
 #define NUMDIGIT 10
 
 /**
+ * findMaxPower10 - find the max value in `array` and return the number
+ * of significant digits in base 10
+ * @array: array to check
+ * @size: size of `array`
+ *
+ * Return: number of significant digits of max value in `array`
+ */
+size_t findMaxPower10(int *array, size_t size)
+{
+	size_t i, base10;
+	int max = array[0], mult;
+
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	for (mult = 1, base10 = 0; max > mult; mult *= 10, base10++)
+		;
+	return (base10);
+}
+
+
+/**
  * radix_sort - sorts data using Radix and prints it out
  * @array: data to be sorted
  * @size: size of the array
  */
 void radix_sort(int *array, size_t size)
 {
-	size_t max, digit, divisor = 1, base10 = 0, z, mult,  *output;
-	size_t *count, i, j, item, oldCount;
+	size_t digit, divisor, base10, i, oldCount, z, *output, count[NUMDIGIT];
 
 	if ((array == NULL) || (size < 2))
 		return;
-	count = malloc(sizeof(size_t) * NUMDIGIT);
-        if (count == NULL)
-                return;
 	output = malloc(sizeof(int) * size);
 	if (output == NULL)
-	{
-		free(count);
 		return;
-	}
-
-	for (i = 0; i < size; i++)
-		output[i] = 0;
 	for (i = 0; i < NUMDIGIT; i++)
 		count[i] = 0;
-
-	max = array[0];
-	/* init arrays and find max array element */
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-			max = array[i];
-	}
-	/* Find largest power of 10 in max number */
-	for (mult = 1; max > mult; mult *= 10, base10++)
-		;
-
-	/* Calc significant digits */
-	for (z = 0; z < base10; z++)
+	base10 = findMaxPower10(array, size);
+	for (z = 0, divisor = 1; z < base10; z++, divisor *= 10)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -54,28 +54,17 @@ void radix_sort(int *array, size_t size)
 			oldCount = count[i - 1];
 			count[i] = oldCount + count[i];
 		}
-		printf("COUNT:");
-		for (i = 0; i < NUMDIGIT; i++)
-			printf("%lu ", count[i]);
-		printf("\n");
 		for (i = size; i > 0; i--)
 		{
 			digit = (array[i - 1] / divisor) % 10;
 			output[count[digit] - 1] = array[i - 1];
 			count[digit]--;
 		}
-
 		for (i = 0; i < size; i++)
-		{
 			array[i] = output[i];
-			output[i] = 0;
-		}
 		for (i = 0; i < NUMDIGIT; i++)
 			count[i] = 0;
 		print_array(array, size);
-		divisor *= 10;
 	}
-
-	/*free(count);
-	  free(output);*/
+	free(output);
 }
